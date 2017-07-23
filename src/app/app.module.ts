9905717c492dd6d66ebe3d1d314719eb4a6428d1
Antonio Schonmann <antonio_schonmann@hotmail.com>
2017-07-22 21:29:47 -0300

    Plugin added! Added ng2-translate plugin for multi language support;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms'
+import { HttpModule, Http } from '@angular/http'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (http: Http) => new TranslateStaticLoader(http, '/assets/i18n', '.json'),
+      deps: [Http],
+    }),
+    BrowserAnimationsModule,
+
+    /* Angular Material Modules. */
+    
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
